6654d1d127d8b1a641b50e8648140ab123bd1a0a
jasmin <pimsupa_jasmin@ku.edu>
2021-09-01 15:49:21 -0500

    make noise_loc an argument

--- code/Figure2.R
+++ code/Figure2.R
@@ -1,4 +1,5 @@
 source("./simsPlot_fxn.R")
+noise_loc <- "../results_numeric/noise.RData"
 fig_loc <- "../results_figs/"
 if(dir.exists(fig_loc)==FALSE){
   dir.create(fig_loc)
@@ -10,7 +11,7 @@ fig2data_loc <- "../results_numeric/fig2data.RDS"
 pdf(fig2_loc) 
 par(mfrow=c(2,1), mar=c(3,3,1,1), oma=c(2,1,1,1))
 
-fig2 <- simsPlot(mudif=-0.8, delta=0.8, sigma=5, end=1000) # r_woATA = 0.098, r = -0.0373, [E||C] = -0.1354
+fig2 <- simsPlot(noise_loc, mudif=-0.8, delta=0.8, sigma=5, end=1000) # r_woATA = 0.098, r = -0.0373, [E||C] = -0.1354
 
 dev.off()
 saveRDS(fig2, file=fig2data_loc)
