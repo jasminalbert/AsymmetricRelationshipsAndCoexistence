b7aabdc060308de984a0c7b9e0dcac38e8fdf2f4
jasmin <pimsupa_jasmin@ku.edu>
2021-09-01 16:02:13 -0500

    make noise_loc an argument

--- code/decomp1_plotting.R
+++ code/decomp1_plotting.R
@@ -1,8 +1,8 @@
 #THIS IS A FUNCTION
 source("./decomposition_fxn.R")
 
-dePlot1 <- function(mudif, delta, qij=FALSE, legend=FALSE,...){
-  load("../results_numeric/noise_etc.RData")
+dePlot1 <- function(noise_loc, mudif, delta, qij=FALSE, legend=FALSE,...){
+  load(noise_loc)
   store <- vector(mode='list', length=7)
   sigma <- seq(0,7,1)
   
@@ -45,26 +45,3 @@ dePlot1 <- function(mudif, delta, qij=FALSE, legend=FALSE,...){
 }
 
 
-#dePlot1(-0.3,1.0)
-
-
-
-
-
-#x0 <- decomp(0.8, c(0.3,0.3), 0.5, M)
-#x1 <- decomp(1.6, c(0.3,0.3), 0.5, M)
-#x2 <- decomp(3.2, c(0.3,0.3), 0.5, M)
-
-#plot(0, xlab="", ylab="", ylim=c(-0.08,0.35), xlim=c(0.8,3.2), col="white")
-#polygon(c(0.8,0.8,1.6,3.2,3.2),c(0,x0$DEC_[1],x1$DEC_[1], x2$DEC_[1],0)
-#,col="darkred", border=NA)
-#polygon(c(0.8,0.8,1.6,3.2,3.2),c(0,x0$Dnull[1],x1$Dnull[1], x2$Dnull[1],0)
-#,col="darkblue", border=NA)
-#polygon(c(0.8,0.8,1.6,3.2,3.2),c(0,x0$DE[1],x1$DE[1], x2$DE[1],0)
-#,col="darkorange1", border=NA)
-#polygon(c(0.8,0.8,1.6,3.2,3.2),c(0,x0$DC[1],x1$DC[1], x2$DC[1],0)
-#,col="gold", border=NA)
-#polygon(c(0.8,0.8,1.6,3.2,3.2),c(0,x0$DECsharp[1],x1$DECsharp[1], x2$DECsharp[1],0)
-#,col="darkgreen", border=NA)
-#lines(c(0.8,1.6,3.2), c(sum(x0[1,]), sum(x1[1,]), sum(x2[1,])), lwd=3)
-#abline(h=0, lty=2)
