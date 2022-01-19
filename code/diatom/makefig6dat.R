22b20f80a89d2b47ad3328e4c63b9047f9606dc0
jasmin <pimsupa_jasmin@ku.edu>
2022-01-19 11:34:21 -0600

    fixed source of error:Tbar seq arguemnts were out of range

--- code/diatom/makefig6dat.R
+++ code/diatom/makefig6dat.R
@@ -10,8 +10,8 @@ if(dir.exists(dat_loc)==FALSE){
 a <- seq(3.5,6,length.out=100)
 P <- seq(51,199.5,length.out=100)
 Tb <- 18
-
 parmlist <- list(a,P, Tb)
+
 aP <- mapspace(parmlist, sims=200, time=3000) #makes dataframe 
 aP <- cmMap(aP)
 saveRDS(aP, paste0(dat_loc,'aP.RDS'))
@@ -19,9 +19,9 @@ saveRDS(aP, paste0(dat_loc,'aP.RDS'))
 # amplitude x Tbar
 a <- seq(3.5,6,length.out=100)
 P<- 60
-Tb <- seq(50, 220, length.out=100)
+Tb <- seq(12, 24, length.out=100)
+parmlist <- list(a[1:5],P, Tb[1:5])
 
-parmlist <- list(a,P, Tb)
 aTb <- mapspace(parmlist, sims=200, time=3000)
 aTb <- cmMap(aTb)
 saveRDS(aTb, paste0(dat_loc,'aTb.RDS'))
@@ -29,9 +29,9 @@ saveRDS(aTb, paste0(dat_loc,'aTb.RDS'))
 # Tbar x period
 a <- 6
 P <- seq(51,199.5,length.out=100)
-Tb <- seq(50, 220, length.out=100)
-
+Tb <- seq(12, 24, length.out=100)
 parmlist <- list(a,P, Tb)
+
 pTb <- mapspace(parmlist, sims=200, time=3000)
 pTb <- t(cmMap(pTb))
 saveRDS(pTb, paste0(dat_loc,'pTb.RDS'))
