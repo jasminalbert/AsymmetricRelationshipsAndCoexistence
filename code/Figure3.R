02b42eebe680c268dc3217a4f07bfb4ff88a3f37
jasmin <pimsupa_jasmin@ku.edu>
2022-01-27 13:45:17 -0600

    formatting

--- code/Figure3.R
+++ code/Figure3.R
@@ -32,7 +32,7 @@ midy <- (ph*(nm/2))/sum(ht) #middle of figure panels; vertical
 #########################################################################################
 pdf(fig3_loc)
 
-par(mgp=c(3,0.5,0), mar = c(1,1,1,1), oma=c(3,5,3,2), xpd=TRUE)
+par(mgp=c(3,0.6,0), mar = c(0.5,1,1,1), oma=c(4,4,2,2), xpd=NA)
 
 layout(matrix(c(2,3,4,5,1,
                 6,7,8,9,1,	
@@ -48,8 +48,8 @@ legend("topright",
                 expression(Delta[i]^"[E||C]"), expression(Delta[i]^"[EC]")
                 ,expression(IGR)), 
        col = c("black","black","black","black","blue","red","orange"),
-       lty = c(1,2,4,3,1,1,1), bty="n", cex=1.5, inset=c(0.1,-0.042),
-       y.intersp = 1.25, x.intersp = 0.1, seg.len=1)
+       lty = c(1,2,4,3,1,1,1), bty="n", cex=1.8, inset=c(0,-0.05),
+       y.intersp = 1.1, x.intersp = 0.1, seg.len=0.8, lwd=1.5)
 #2-5
 for (d in 1:nd){
   plot.new()
@@ -60,24 +60,24 @@ res <- vector(mode='list',length=1)
 n <- 1
 for (m in 1:nm){
   for (d in 1:nd){
-    res <- append(res,dePlot1(noise_loc, mudif[m], delta[d], xaxt="n"))
-    axis(1, labels=ifelse(n>12, yes=TRUE, no=FALSE), tick=TRUE)
-    mtext(paste0("(", letters[n],")"), side=3, line=-1.45, at=0.7)
+    res <- append(res,dePlot1(noise_loc, mudif[m], delta[d], xaxt="n", cex.axis=1.2))
+    axis(1, labels=ifelse(n>12, yes=TRUE, no=FALSE), tick=TRUE, cex.axis=1.3)
+    mtext(paste0("(", letters[n],")"), side=3, line=-1.7, at=0, cex=1.3, adj=0)
     
     if(n<5){
-      mtext(paste(delta[d]), side=3, line=0.75, font=2, cex=0.8)
+      mtext(paste(delta[d]), side=3, line=0.2, font=2, cex=1)
     }
     if(n%%4==0){
-      mtext(paste(mudif[m]), side=4, line=0.75, font=2, cex=0.8)
+      mtext(paste(mudif[m]), side=4, line=0.6, font=2, cex=1)
     }
     n <- n+1
   }
 }
 
-mtext("contribution to coexistence", side=2, outer=TRUE, line=1.5, font=2, cex=1, at=midy)
-mtext(expression(mu[1]-mu[2]), side=4, outer=TRUE, line=-5, font=2, cex=1.5, at=midy)
-mtext(expression(delta), side=3, outer=TRUE, line=-2, font=2, cex=1.5, at=midx)
-mtext(expression(sigma), outer=TRUE, side=1, line=1, cex.lab=1.3, at=midx)
+mtext("contribution to coexistence", side=2, outer=TRUE, line=1.3, font=2, cex=1.3, at=midy-0.005)
+mtext(expression(mu[1]-mu[2]), side=4, outer=TRUE, line=-5.5, font=2, cex=2, at=midy)
+mtext(expression(delta), side=3, outer=TRUE, line=-2.5, font=2, cex=2, at=midx)
+mtext(expression(sigma), outer=TRUE, side=1, line=2, cex=1.5, at=midx)
 
 dev.off()
 fig3maxse <- max(unlist(lapply(res, function(X){X$D_se})), na.rm = TRUE)
@@ -88,7 +88,7 @@ saveRDS(fig3maxse, file=fig3maxse_loc)
 #####################################################################################
 pdf(fig3qij_loc)
 
-par(mgp=c(3,0.5,0), mar = c(1,1,1,1), oma=c(3,5,3,2))
+par(mgp=c(3,0.6,0), mar = c(0.5,1,1,1), oma=c(4,4,2,2), xpd=NA)
 
 layout(matrix(c(2,3,4,5,1,
                 6,7,8,9,1,	
@@ -104,8 +104,8 @@ legend("topright",
                 expression(Delta[i]^"[E||C]"), expression(Delta[i]^"[EC]")
                 ,expression(IGR)), 
        col = c("black","black","black","black","blue","red","orange"),
-       lty = c(1,2,4,3,1,1,1), bty="n", cex=1.5, inset=c(0.1,-0.025),
-       y.intersp = 1.25, x.intersp = 0.1, seg.len=1)
+       lty = c(1,2,4,3,1,1,1), bty="n", cex=1.8, inset=c(0,-0.05),
+       y.intersp = 1.1, x.intersp = 0.1, seg.len=0.8, lwd=1.5)
 #2-5
 for (d in 1:nd){
   plot.new()
@@ -116,24 +116,24 @@ res <- vector(mode='list',length=1)
 n <- 1
 for (m in 1:nm){
   for (d in 1:nd){
-    res <- append(res,dePlot1(noise_loc, mudif[m], delta[d], qij=TRUE, xaxt="n"))
-    axis(1, labels=ifelse(n>12, yes=TRUE, no=FALSE), tick=TRUE)
-    mtext(paste0("(", letters[n],")"), side=3, line=-1.45, at=0.7)
+    res <- append(res,dePlot1(noise_loc, mudif[m], delta[d], qij=TRUE, xaxt="n", cex.axis=1.2))
+    axis(1, labels=ifelse(n>12, yes=TRUE, no=FALSE), tick=TRUE, cex.axis=1.3)
+    mtext(paste0("(", letters[n],")"), side=3, line=-1.7, at=0, cex=1.3, adj=0)
     
     if(n<5){
-      mtext(paste(delta[d]), side=3, line=0.75, font=2, cex=0.8)
+      mtext(paste(delta[d]), side=3, line=0.2, font=2, cex=1)
     }
     if(n%%4==0){
-      mtext(paste(mudif[m]), side=4, line=0.75, font=2, cex=0.8)
+      mtext(paste(mudif[m]), side=4, line=0.6, font=2, cex=1)
     }
     n <- n+1
   }
 }
 
-mtext("contribution to coexistence", side=2, outer=TRUE, line=1.5, font=2, cex=1, at=midy)
-mtext(expression(mu[1]-mu[2]), side=4, outer=TRUE, line=-5, font=2, cex=1.5, at=midy)
-mtext(expression(delta), side=3, outer=TRUE, line=-2, font=2, cex=1.5, at=midx)
-mtext(expression(sigma), outer=TRUE, side=1, line=1, cex.lab=1.3, at=midx)
+mtext("contribution to coexistence", side=2, outer=TRUE, line=1.3, font=2, cex=1.3, at=midy-0.005)
+mtext(expression(mu[1]-mu[2]), side=4, outer=TRUE, line=-5.5, font=2, cex=2, at=midy)
+mtext(expression(delta), side=3, outer=TRUE, line=-2.5, font=2, cex=2, at=midx)
+mtext(expression(sigma), outer=TRUE, side=1, line=2, cex=1.5, at=midx)
 
 dev.off()
 fig3qijmaxse <- max(unlist(lapply(res, function(X){X$Dq_se})), na.rm = TRUE)
