4d6ef193628161d2f81c13638c3cedfb864f1780
jasmin <pimsupa_jasmin@ku.edu>
2022-01-27 14:48:48 -0600

    fixed line color

--- code/diatom/Figure5.R
+++ code/diatom/Figure5.R
@@ -18,43 +18,47 @@ vTbr <- readRDS(paste(dat_loc,'varyTbar.RDS',sep=''))
 
 col <- c(rep("black",4), "red", "blue", "orange")
 line <- c(1:4, 1,1,1)
+lwd <- c(rep(2,6),3)
 
 varylist <- list(vAmp, vPer, vTbr)
 range <- sapply(varylist, function(X){range(X[,1:7])})
 ymin <- min(range)
 ymax <- max(range)
 orig <- c(a=6, P=60, Tbar=18)
+xlab <- c("a", "P", expression(theta[0]))
 
 #default is c(5, 4, 4, 2) + 0.1
 pdf(fig5, height=5, width=15)
-par(mfrow=c(1,3), oma=c(0,3,0,0), mar=c(5,1,2,1) )
+par(mfrow=c(1,3), oma=c(0,4,0,0), mar=c(5,1,2,1) )
 for (i in 1:3){
-	vdat <- varylist[[i]]
-	
-	#empty box
-	plot(0, yaxt='n', xlim=range(vdat[,10]), ylim=c(ymin, ymax), col='white', xlab=colnames(varylist[[i]])[10], ylab='', cex.lab=1.3, cex.axis=1.3)
-	
-	#axis
+  vdat <- varylist[[i]]
+  
+  #empty box
+  plot(0, yaxt='n', xlim=range(vdat[,10]), ylim=c(ymin, ymax), col='white', xlab="", ylab='', cex.lab=1.8, cex.axis=1.8)
+  title(xlab=xlab[i], cex.lab=2.3, line=3.5)
+  #axis
+  if (i==1){
+    axis(2, cex.axis=1.8)
+  }
+  
+  #loop for lines
+  for (j in 1:7){
+    lines(vdat[,10], vdat[,j], col=col[j], lty=line[j], lwd=lwd[j])	
+  }
+  abline(h=0, col="lightgrey") #zero
+  abline(v=orig[i], col='magenta', lty=3, lwd=2)
+  
+  #label
+  mtext(paste0("(", letters[i],")"), 3, -2.5, adj=0.985, cex=1.8)
+  
+  #legend
 	if (i==1){
-		axis(2)
-	}
-	
-	#loop for lines
-	for (j in 1:7){
-		lines(vdat[,10], vdat[,j], col=col[j], lty=line[j])	
-	}
-	abline(h=0, col="lightgrey") #zero
-	abline(v=orig[i], col='magenta', lty=3)
-	
-	#label
-	mtext(paste0("(", letters[i],")"), 3, -1.5, adj=0.985)
-	
-	#legend
-	if (i==1){
-		legend("topleft", legend=c(expression(Delta[i]^0), expression(Delta[i]^E), 				expression(Delta[i]^C), expression(Delta[i]^"(E#C)"), expression(Delta[i]^"[E||C]"), expression(Delta[i]^"[EC]") ,expression(IGR)), col = col,lty = line, bty="n", cex=1.2, inset=c(0,0))
+		legend("topleft", legend=c(expression(Delta[i]^0), expression(Delta[i]^E), expression(Delta[i]^C), expression(Delta[i]^"(E#C)"), expression(Delta[i]^"[E||C]"), expression(Delta[i]^"[EC]") ,expression(IGR)), 
+		       col = c(rep("black",4), "blue", "red", "orange"),lty = line, bty="n", cex=1.9, inset=c(-0.02,-0.03), 
+		       y.intersp = 1.1, x.intersp = 0.1, seg.len=0.8, lwd=1.8)
 	}
 }
-title(ylab="coexistence", outer=T, line=1.5, cex.lab=1.3)
+title(ylab="contribution to coexistence", outer=T, line=2.1, cex.lab=2, font.lab=2)
 
 dev.off()
 
