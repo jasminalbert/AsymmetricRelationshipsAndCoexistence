2a0cf1f4ea98883abf0e72acd93608df6b7a753f
jasmin <pimsupa_jasmin@ku.edu>
2022-02-16 11:06:26 -0600

    commented out fig6_2

--- code/diatom/SuppFigs5and6.R
+++ code/diatom/SuppFigs5and6.R
@@ -28,15 +28,15 @@ fig5(fig5_2, dat_loc, invader=2) #makes figure
 # SI version of fig 6 #
 #######################
 # make data here
-dat_loc <- "../results_numeric/fig6dat2/"
-if (dir.exists(dat_loc)==FALSE){
-  dir.create(dat_loc)
-  a <- seq(3.5,6,length.out=100)
-  P <- seq(51,199.5,length.out=100)
-  Tbar <- seq(16,19,length.out=100)
-  mak6(dat_loc, a_vec=a, P_vec=P, T_vec=Tbar, parms)
-}
-fig6_2 <- paste0(fig_loc,"fig6_2.pdf")
+#dat_loc <- "../results_numeric/fig6dat2/"
+#if (dir.exists(dat_loc)==FALSE){
+#  dir.create(dat_loc)
+#  a <- seq(3.5,6,length.out=100)
+#  P <- seq(51,199.5,length.out=100)
+#  Tbar <- seq(16,19,length.out=100)
+#  mak6(dat_loc, a_vec=a, P_vec=P, T_vec=Tbar, parms)
+#}
+#fig6_2 <- paste0(fig_loc,"fig6_2.pdf")
 
 fig6(fig6_2, dat_loc, invader=2) #makes figure
 
