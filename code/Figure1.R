45782f05ebcc7cab8189c681d0a5f42a21e37e2a
jasminalbert <pimsupa_jasmin@ku.edu>
2023-04-27 13:07:35 -0700

    rearrange columns

--- code/Figure1.R
+++ code/Figure1.R
@@ -35,16 +35,26 @@ grDevices::pdf(fig1_loc, width=7.5, height=5)
 
 graphics::par(mar=c(0.5,0.5,0,0), oma=c(4,3,2,1),mgp=c(2.2,0.6,0), xpd=NA, cex.lab=1.7)
 
+#graphics::layout(matrix(c(
+#				2 ,13,13, 4,14,15,
+ #               1 ,3 ,13, 4,14,15,
+  #              6 ,13,13, 8,14,15,
+   #             5 ,7, 13, 8,14,15,
+    #            5 ,7, 13, 8,14,13,
+     #           5 ,7, 13, 8,14,16,
+      #          10,13,13,12,14,16,
+       #         9,11,13,12,14,16),nrow=8,byrow=T),
+       #heights=c(0.22,1,0.22,0.36,0.03,0.61,0.22,1), widths=c(1,0.25,0.4,2.5,0.45,1.85))
 graphics::layout(matrix(c(
-				2 ,13,13, 4,14,15,
-                1 ,3 ,13, 4,14,15,
-                6 ,13,13, 8,14,15,
-                5 ,7, 13, 8,14,15,
-                5 ,7, 13, 8,14,13,
-                5 ,7, 13, 8,14,16,
-                10,13,13,12,14,16,
-                9,11,13,12,14,16),nrow=8,byrow=T),
-       heights=c(0.22,1,0.22,0.36,0.03,0.61,0.22,1), widths=c(1,0.25,0.4,2.5,0.45,1.85))
+				2 ,13,13, 15,14,4,
+                1 ,3 ,13, 15,14,4,
+                6 ,13,13, 15,14,8,
+                5 ,7, 13, 15,14,8,
+                5 ,7, 13, 13,14,8,
+                5 ,7, 13, 16,14,8,
+                10,13,13,16,14,12,
+                9,11,13,16,14,12),nrow=8,byrow=T),
+       heights=c(0.22,1,0.22,0.36,0.03,0.61,0.22,1), widths=c(1,0.25,0.4,1.85,0.45,2.5))       
 
 for (panel in 1:3){
   
@@ -71,19 +81,19 @@ for (panel in 1:3){
   graphics::par(mar=c(0.5,0.25,1,0))
   graphics::plot(popsim[[panel]]$N1[1:250], type="l", ylim=c(0,50), xlab=ifelse(panel==3,"time",NA), ylab=NA, xaxt=ifelse (panel==3,"s","n"), bty="l", lwd=1.5)
   graphics::title(ylab=ifelse(panel==2,"species 1 population",NA), line=1.7)
-  graphics::mtext(paste0("(",letters[panel+3],")"), side=3, line=-0.8, at=7)
+  graphics::mtext(paste0("(",letters[panel+5],")"), side=3, line=-0.8, at=7)
 }
 
 graphics::par(mar=c(0.5,0.25,0.3,0))
 plot.new();plot.new()
 # plankton example - ranked density
-title(ylab="C. furca density", line=-1.6)
+title(ylab="C. furca density", line=17)
 for (panel in 1:2){
 	cerfus <- ceratium1x2[[panel]]$cerfus
 	cerfur <- ceratium1x2[[panel]]$cerfur
 	Tot <- dim(ceratium1x2[[panel]])[1]
 	graphics::plot(rank(cerfus)/Tot, rank(cerfur)/Tot, bty="n",pch=20, col="darkgrey", cex=2, ylim=c(0,1),xlim=c(0,1), xlab=ifelse(panel==2,"C. fusus density",NA), ylab=NA, xaxt=ifelse(panel==2,"s","n"))
-	graphics::mtext(paste0("(",letters[panel+6],")"), side=3, line=-1.5, at=0.05)
+	graphics::mtext(paste0("(",letters[panel+3],")"), side=3, line=-1.5, at=0.05)
 }
 #mtext("C. furca density", side=4,outer=T, line=-14, cex=1)
 
