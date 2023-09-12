69368f47fbca3182fb1296f009202ecd3ec55e32
jasminalbert <pimsupaa@uoregon.edu>
2023-09-12 15:40:02 -0700

    invoking libs

--- code/diatom/diatomModel_andVKQ.R
+++ code/diatom/diatomModel_andVKQ.R
@@ -2,8 +2,10 @@
   #1. Data from Gonzalez 2005 diatom coexistence experiment
   #2. Functions derived from those data
   #3. competition model function to put into ode 
+  
+##libraries used (invoked with ::): stats
 
-#1. Data
+#species
 #1 = Fragiliaria crotonensis
 #2 = Cyclotella psuedostelligera
 
@@ -24,27 +26,28 @@ Q2data <-  c(0.301,0.862,0.619,NA)
 
 #2. Functions
 #interpolations
-V1fun <-  approxfun(Tvals,V1data,rule = 2) 
-V2fun <-  approxfun(Tvals,V2data,rule = 2)
+V1fun <-  stats::approxfun(Tvals,V1data,rule = 2) 
+V2fun <-  stats::approxfun(Tvals,V2data,rule = 2)
 
-K1fun <-  approxfun(Tvals,K1data,rule = 2) 
-K2fun <-  approxfun(Tvals,K2data,rule = 2) 
+K1fun <-  stats::approxfun(Tvals,K1data,rule = 2) 
+K2fun <-  stats::approxfun(Tvals,K2data,rule = 2) 
 
-Q1fun <-  approxfun(Tvals,Q1data,rule = 2)
-Q2fun <-  approxfun(Tvals,Q2data,rule = 2)
+Q1fun <-  stats::approxfun(Tvals,Q1data,rule = 2)
+Q2fun <-  stats::approxfun(Tvals,Q2data,rule = 2)
 
 #modifications
-fitV1 <-  lm(V1data~Tvals+I(Tvals^2)); c1 <-  coef(fitV1); 
+fitV1 <-  stats::lm(V1data~Tvals+I(Tvals^2)); 
+c1 <-  stats::coef(fitV1); 
 V1quad <-  function(temp) c1[1] + c1[2]*temp + c1[3]*temp^2
 
 K1flat <-  c(0.06,0.25,0.25,0.25); 
-K1flatfun<- approxfun(Tvals,K1flat,rule = 2); 
+K1flatfun<- stats::approxfun(Tvals,K1flat,rule = 2); 
 
 K2flat <-  c(0.07,0.14,0.14,0.14); 
-K2flatfun<- approxfun(Tvals,K2flat,rule = 2);
+K2flatfun<- stats::approxfun(Tvals,K2flat,rule = 2);
 
 V2dataMod <-  c(0.32,0.36,0.39,0.06)   # Cyclotella data with last value modified 
-V2modfun <-  approxfun(Tvals,V2dataMod,rule = 2)
+V2modfun <-  stats::approxfun(Tvals,V2dataMod,rule = 2)
 
 Q2mod <- c(0.301,0.862,0.619,0.619)
 
